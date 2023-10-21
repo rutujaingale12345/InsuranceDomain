7b1720efb5ca5360a338390f0acf6345f8fff8fa
Rutuja <rutujaingale347@gmail.com>
2023-10-21 18:35:56 +0530

    R123 - Insurance

---
+++ src/main/java/com/base/Baseclass.java
@@ -0,0 +1,24 @@
+package com.base;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Baseclass {
+	public WebDriver driver;
+
+	public void LaunchTheWeb() throws IOException{
+		FileInputStream fileInputStream = new FileInputStream(".\\src\\main\\resources\\property");
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Properties properties=new Properties();
+		properties.getProperty("weburl");
+		driver.get(properties.getProperty("weburl"));
+		
+	}
+
+}
